fb67aa248dfc0511e272c8be9c45e41d1388afd0
Nikorasu <nikorasu85@gmail.com>
2023-02-22 23:40:00 -0600

    Cleaned up commented-out unnecessary junk & notes.

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-#from gc import collect
 from framebuf import FrameBuffer, GS8, RGB565
 from machine import ADC, Pin, SPI
 from ssd1351 import Display, color565
-#from time import sleep_us, ticks_us, ticks_diff
 
 def thumb2dir(thumbstick):
     x, y = thumbstick[0].read_u16(), thumbstick[1].read_u16()
-    #x2 = (x-32768)/32768 if abs(x-32768) > 1000 else 0 # 1000 is deadzone
-    #y2 = (y-32768)/32768 if abs(y-32768) > 1000 else 0 # following is improved:
-    #y2 = 0 if abs(x-32768) < 1000 and abs(y-32768) < 1000 else (y-32768)/32768
     dir = (0,0) if abs(x-32768) < 1000 and abs(y-32768) < 1000 else ((x-32768)/32768,(y-32768)/32768)
     return dir
 
@@ -34,19 +29,14 @@ class Eye:
         curlids = bytearray(self.size[0] * self.size[1])
         for i in range(len(self.lids)):
             curlids[i] = 0xFF if self.lids[i] > self.blinklvl else 0x00
-        
         lidsfb = FrameBuffer(curlids, self.size[0], self.size[1], GS8)
         
         self.fb.fill(65503)
-        
         self.fb.ellipse(64+int(look[0]*24),64+int(look[1]*24),39,39,16706,True)
         self.fb.ellipse(64+int(look[0]*24),64+int(look[1]*24),15,15,0,True)
         
         self.fb.blit(lidsfb,0,0,0xFF,GS8)
         self.display.block(0,0,self.size[0]-1,self.size[1]-1,self.buffer)
-        
-        #del fb, curlids, lidsfb
-        #collect()
 
 def main():
     screensize = (128,128) # currently only supports 128x128
@@ -55,22 +45,12 @@ def main():
     display = Display(spi, dc=Pin(16), cs=Pin(17), rst=Pin(20))
 
     thumbstick = (ADC(28), ADC(27), Pin(26,Pin.IN,Pin.PULL_UP))
-    # thumbstick[2].value() == 0 if button is pressed
-    
     eye1 = Eye(display, screensize)
     
     try:
         while True:
-            #timer = ticks_us()
-            
             if thumbstick[2].value() == 0: eye1.blinking = True
-
             eye1.update(thumb2dir(thumbstick))
-            
-            # Attempt to set framerate to 30 FPS
-            #timer_dif = 33333 - ticks_diff(ticks_us(), timer)
-            #if timer_dif > 0:
-            #    sleep_us(timer_dif)
 
     finally:
         display.cleanup()
